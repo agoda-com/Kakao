550d013246e04e94aa3facc15f55a01800ea242f
Ju Tipatai <tipatai.puthanukunkit@agoda.com>
2017-10-12 17:43:23 +0700

    fix findViewById to be in support 26

--- sample/src/main/kotlin/com/agoda/sample/WebAcitivty.kt
+++ sample/src/main/kotlin/com/agoda/sample/WebAcitivty.kt
@@ -9,7 +9,7 @@ class WebAcitivty : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
-        val webView = (findViewById(R.id.webView) as WebView).apply {
+        val webView = (findViewById<WebView>(R.id.webView)).apply {
             settings.javaScriptEnabled = true
         }
         webView.loadData("""
