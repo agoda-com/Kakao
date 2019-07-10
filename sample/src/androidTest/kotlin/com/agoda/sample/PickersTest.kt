c31ae715cbba04b6d8a366b53cd101c0bad92159
Konstantin Aksenov <aksenov.kostya@gmail.com>
2019-07-10 12:51:36 +1000

    Add test for picker dialogs

--- sample/src/androidTest/kotlin/com/agoda/sample/PickersTest.kt
+++ sample/src/androidTest/kotlin/com/agoda/sample/PickersTest.kt
@@ -1,2 +1,45 @@
 package com.agoda.sample
 
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import androidx.test.rule.ActivityTestRule
+import com.agoda.kakao.screen.Screen
+import com.agoda.sample.screen.PickersActivityScreen
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class PickersTest {
+    @Rule
+    @JvmField
+    val rule = ActivityTestRule(PickersActivity::class.java)
+
+    @Test
+    fun testTextInputLayout() {
+        Screen.onScreen<PickersActivityScreen> {
+            selectTimeButton {
+                click()
+            }
+
+            timePicker {
+                setTime(22, 4)
+            }
+
+            timeText {
+                hasText("22:4")
+            }
+
+            selectDateButton {
+                click()
+            }
+
+            datePicker {
+                setDate(1955, 11, 12)
+            }
+
+            dateText {
+                hasText("12 11 1955")
+            }
+        }
+    }
+}
