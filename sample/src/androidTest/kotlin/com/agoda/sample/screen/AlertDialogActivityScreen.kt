5821334382d9a41ba8b1fbf31f16fd8b3dda7143
Konstantin Aksenov <aksenov.kostya@gmail.com>
2019-07-30 12:21:11 +1000

    Add Kakao Screen for Alerd Dialog

--- sample/src/androidTest/kotlin/com/agoda/sample/screen/AlertDialogActivityScreen.kt
+++ sample/src/androidTest/kotlin/com/agoda/sample/screen/AlertDialogActivityScreen.kt
@@ -1,25 +1,12 @@
 package com.agoda.sample.screen
 
 import com.agoda.kakao.common.views.KView
-import com.agoda.kakao.picker.date.KDatePickerDialog
-import com.agoda.kakao.picker.time.KTimePickerDialog
-
+import com.agoda.kakao.dialog.KAlertDialog
 import com.agoda.kakao.screen.Screen
-import com.agoda.kakao.text.KTextView
 import com.agoda.sample.R
 
 open class AlertDialogActivityScreen : Screen<AlertDialogActivityScreen>() {
-    val selectDateButton: KView = KView { withId(R.id.select_date) }
-    val selectTimeButton: KView = KView { withId(R.id.select_time) }
+    val showAlertDialogButton = KView { withId(R.id.show_alert_dialog) }
 
-    val dateText: KTextView = KTextView {
-        withId(R.id.date_field)
-    }
-
-    val timeText: KTextView = KTextView {
-        withId(R.id.time_field)
-    }
-
-    val datePickerDialog: KDatePickerDialog = KDatePickerDialog()
-    val timePickerDialog: KTimePickerDialog = KTimePickerDialog()
+    val alertDialog = KAlertDialog()
 }
