8d6849324108ab9c655eb9845ab1baafd59bea0f
Vyacheslav Rodionov <rodionovSlava95@gmail.com>
2019-09-28 01:03:05 +0300

    supporting EditText and child realization for KTextInputLayout

--- kakao/src/main/kotlin/com/agoda/kakao/edit/KTextInputLayout.kt
+++ kakao/src/main/kotlin/com/agoda/kakao/edit/KTextInputLayout.kt
@@ -3,10 +3,11 @@
 package com.agoda.kakao.edit
 
 import android.view.View
+import android.widget.EditText
 import androidx.test.espresso.DataInteraction
+import androidx.test.espresso.matcher.ViewMatchers
 import com.agoda.kakao.common.builders.ViewBuilder
 import com.agoda.kakao.common.views.KBaseView
-import com.google.android.material.textfield.TextInputEditText
 import org.hamcrest.Matcher
 
 /**
@@ -20,21 +21,21 @@ class KTextInputLayout : KBaseView<KTextInputLayout>, TextInputLayoutAssertions 
     constructor(function: ViewBuilder.() -> Unit) : super(function) {
         edit = KEditText {
             isDescendantOfA(function)
-            isInstanceOf(TextInputEditText::class.java)
+            withMatcher(ViewMatchers.isAssignableFrom(EditText::class.java))
         }
     }
 
     constructor(parent: Matcher<View>, function: ViewBuilder.() -> Unit) : super(parent, function) {
         edit = KEditText {
             isDescendantOfA(function)
-            isInstanceOf(TextInputEditText::class.java)
+            withMatcher(ViewMatchers.isAssignableFrom(EditText::class.java))
         }
     }
 
     constructor(parent: DataInteraction, function: ViewBuilder.() -> Unit) : super(parent, function) {
         edit = KEditText {
             isDescendantOfA(function)
-            isInstanceOf(TextInputEditText::class.java)
+            withMatcher(ViewMatchers.isAssignableFrom(EditText::class.java))
         }
     }
 }
