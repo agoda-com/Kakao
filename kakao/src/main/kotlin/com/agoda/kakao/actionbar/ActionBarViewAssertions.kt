75db39eb425b9aa66635856e63981ecac8be69bb
Vacxe <aksenov.kostya@gmail.com>
2019-11-27 02:11:26 -0800

    Init actionbar support

---
+++ kakao/src/main/kotlin/com/agoda/kakao/actionbar/ActionBarViewAssertions.kt
@@ -0,0 +1,14 @@
+@file:Suppress("unused")
+
+package com.agoda.kakao.actionbar
+
+import androidx.test.espresso.ViewAssertion
+import com.agoda.kakao.common.assertions.BaseAssertions
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+/**
+ * Provides assertion for ActionBar
+ */
+interface ActionBarViewAssertions : BaseAssertions {
+
+}
