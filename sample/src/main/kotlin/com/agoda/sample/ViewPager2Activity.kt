ee4c52d79796d915aab06c4f4b6c16e360259526
Vacxe <aksenov.kostya@gmail.com>
2020-02-15 17:33:04 -0800

    Add dependency

---
+++ sample/src/main/kotlin/com/agoda/sample/ViewPager2Activity.kt
@@ -0,0 +1,11 @@
+package com.agoda.sample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ViewPager2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_pager2)
+    }
+}
