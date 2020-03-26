ce737ffe7594cf1904fc450ae28c375554646469
Vacxe <aksenov.kostya@gmail.com>
2020-03-26 01:37:26 -0700

    Add id matching

--- kakao/src/main/kotlin/com/agoda/kakao/common/matchers/ToolbarTitleMatcher.kt
+++ kakao/src/main/kotlin/com/agoda/kakao/common/matchers/ToolbarTitleMatcher.kt
@@ -1,15 +1,32 @@
 package com.agoda.kakao.common.matchers
 
 import android.view.View
+import androidx.annotation.StringRes
 import androidx.appcompat.widget.Toolbar
 import androidx.test.espresso.matcher.BoundedMatcher
 import org.hamcrest.Description
 
-class ToolbarTitleMatcher(val title: String) : BoundedMatcher<View, Toolbar>(Toolbar::class.java) {
+class ToolbarTitleMatcher() : BoundedMatcher<View, Toolbar>(Toolbar::class.java) {
+
+    var title: String? = null
+    var resId: Int? = null
+
+    constructor(title: String) : this() {
+        this.title = title
+    }
+
+    constructor(@StringRes resId: Int) : this() {
+        this.resId = resId
+    }
+
     override fun describeTo(desc: Description) {
         desc.appendText("Title is not equals to $title")
     }
 
-    override fun matchesSafely(view: Toolbar?): Boolean =
-        view?.title?.equals(title)?: false
+    override fun matchesSafely(view: Toolbar?): Boolean {
+        if (title == null && resId != null) {
+            title = resId?.let { view?.context?.getString(it) }
+        }
+        return view?.title?.equals(title) ?: false
+    }
 }
