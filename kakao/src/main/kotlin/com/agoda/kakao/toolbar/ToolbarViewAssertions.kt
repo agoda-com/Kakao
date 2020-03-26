6a13747f43634c7232db807a9a33cba70dd5c07f
Vacxe <aksenov.kostya@gmail.com>
2020-03-26 01:37:26 -0700

    Add id matching

--- kakao/src/main/kotlin/com/agoda/kakao/toolbar/ToolbarViewAssertions.kt
+++ kakao/src/main/kotlin/com/agoda/kakao/toolbar/ToolbarViewAssertions.kt
@@ -11,11 +11,40 @@ import com.agoda.kakao.common.matchers.ToolbarTitleMatcher
  * Provides assertion for Toolbar
  */
 interface ToolbarViewAssertions : BaseAssertions {
+
+    /**
+     * Checks if Toolbar has title
+     *
+     * @param title expected title string
+     */
     fun hasTitle(title: String) {
         view.check(ViewAssertions.matches(ToolbarTitleMatcher(title)))
     }
 
+    /**
+     * Checks if Toolbar has subtitle
+     *
+     * @param subtitle expected subtitle string
+     */
     fun hasSubtitle(subtitle: String) {
         view.check(ViewAssertions.matches(ToolbarSubtitleMatcher(subtitle)))
+    }
+
+    /**
+     * Checks if Toolbar has title
+     *
+     * @param resId expected title resource id
+     */
+    fun hasTitle(resId: Int) {
+        view.check(ViewAssertions.matches(ToolbarTitleMatcher(resId)))
+    }
+
+    /**
+     * Checks if Toolbar has subtitle
+     *
+     * @param resId expected subtitle resource id
+     */
+    fun hasSubtitle(resId: Int) {
+        view.check(ViewAssertions.matches(ToolbarSubtitleMatcher(resId)))
     }
 }
