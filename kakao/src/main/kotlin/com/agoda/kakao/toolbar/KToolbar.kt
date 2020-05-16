f84d0032a70647128a1825a5a9fdff970f08459c
Niccolò Forlini <forlinick@gmail.com>
2020-05-16 23:36:39 +0200

    WIP

--- kakao/src/main/kotlin/com/agoda/kakao/toolbar/KToolbar.kt
+++ kakao/src/main/kotlin/com/agoda/kakao/toolbar/KToolbar.kt
@@ -2,25 +2,19 @@
 
 package com.agoda.kakao.toolbar
 
-import android.graphics.Bitmap
-import android.graphics.drawable.Drawable
 import android.view.View
-import androidx.annotation.DrawableRes
 import androidx.test.espresso.DataInteraction
 import androidx.test.espresso.Root
-import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.RootMatchers
-import com.agoda.kakao.common.assertions.BaseAssertions
 import com.agoda.kakao.common.builders.ViewBuilder
-import com.agoda.kakao.common.matchers.ToolbarSubtitleMatcher
-import com.agoda.kakao.common.matchers.ToolbarTitleMatcher
 import com.agoda.kakao.delegate.ViewInteractionDelegate
 import com.agoda.kakao.image.KImageView
+import com.agoda.kakao.pager2.KViewPager2
 import org.hamcrest.Matcher
 
-class KToolbar : ToolbarViewActions, ToolbarViewAssertions, BaseAssertions {
+class KToolbar : ToolbarViewActions, ToolbarViewAssertions {
     val matcher: Matcher<View>
-    private val navigationDrawable: KImageView?
+    override val navigationIcon: KImageView?
 
     override val view: ViewInteractionDelegate
     override var root: Matcher<Root> = RootMatchers.DEFAULT
@@ -29,15 +23,15 @@ class KToolbar : ToolbarViewActions, ToolbarViewAssertions, BaseAssertions {
      * Constructs view class with view interaction from given ViewBuilder
      *
      * @param rootBuilder ViewBuilder which will result in view's interaction
-     * @param itemTypeBuilder Lambda with receiver where you pass your item providers
+     * @param navigationIconBuilder ViewBuilder which will be used to locate the navigation icon
      *
      * @see ViewBuilder
      */
-    constructor(rootBuilder: ViewBuilder.() -> Unit, drawableBuilder: (ViewBuilder.() -> Unit)? = null) {
+    constructor(rootBuilder: ViewBuilder.() -> Unit, navigationIconBuilder: (ViewBuilder.() -> Unit)? = null) {
         val vb = ViewBuilder().apply(rootBuilder)
         matcher = vb.getViewMatcher()
         view = vb.getViewInteractionDelegate()
-        navigationDrawable = drawableBuilder?.let { KImageView(it) }
+        navigationIcon = navigationIconBuilder?.let { KImageView(it) }
     }
 
     /**
@@ -45,31 +39,31 @@ class KToolbar : ToolbarViewActions, ToolbarViewAssertions, BaseAssertions {
      *
      * @param parent Matcher that will be used as parent in isDescendantOfA() matcher
      * @param rootBuilder ViewBuilder which will result in view's interaction
-     * @param itemTypeBuilder Lambda with receiver where you pass your item providers
+     * @param navigationIconBuilder ViewBuilder which will be used to locate the navigation icon
      *
      * @see ViewBuilder
      */
     constructor(
         parent: Matcher<View>, rootBuilder: ViewBuilder.() -> Unit,
-        drawableBuilder: (ViewBuilder.() -> Unit)?
+        navigationIconBuilder: (ViewBuilder.() -> Unit)?
     ) : this({
                  isDescendantOfA { withMatcher(parent) }
                  rootBuilder(this)
-             }, drawableBuilder)
+             }, navigationIconBuilder)
 
     /**
      * Constructs view class with parent and view interaction from given ViewBuilder
      *
      * @param parent DataInteraction that will be used as parent to ViewBuilder
-     * @param builder ViewBuilder which will result in view's interaction
-     * @param drawableBuilder Lambda with receiver where you pass your item providers
+     * @param rootBuilder ViewBuilder which will result in view's interaction
+     * @param navigationIconBuilder ViewBuilder which will be used to locate the navigation icon
      *
      * @see ViewBuilder
      */
     @Suppress("UNCHECKED_CAST")
     constructor(
         parent: DataInteraction, rootBuilder: ViewBuilder.() -> Unit,
-        drawableBuilder: (ViewBuilder.() -> Unit)?
+        navigationIconBuilder: (ViewBuilder.() -> Unit)?
     ) {
         val makeTargetMatcher = DataInteraction::class.java.getDeclaredMethod("makeTargetMatcher")
         val parentMatcher = makeTargetMatcher.invoke(parent)
@@ -81,54 +75,7 @@ class KToolbar : ToolbarViewActions, ToolbarViewAssertions, BaseAssertions {
 
         matcher = vb.getViewMatcher()
         view = vb.getViewInteractionDelegate()
-        navigationDrawable = drawableBuilder?.let { KImageView(it) }
-    }
-
-    /**
-     * Checks if Toolbar has title
-     *
-     * @param title expected title string
-     */
-    fun hasTitle(title: String) {
-        view.check(ViewAssertions.matches(ToolbarTitleMatcher(title)))
-    }
-
-    /**
-     * Checks if Toolbar has subtitle
-     *
-     * @param subtitle expected subtitle string
-     */
-    fun hasSubtitle(subtitle: String) {
-        view.check(ViewAssertions.matches(ToolbarSubtitleMatcher(subtitle)))
-    }
-
-    /**
-     * Checks if Toolbar has title
-     *
-     * @param resId expected title resource id
-     */
-    fun hasTitle(resId: Int) {
-        view.check(ViewAssertions.matches(ToolbarTitleMatcher(resId)))
-    }
-
-    /**
-     * Checks if Toolbar has subtitle
-     *
-     * @param resId expected subtitle resource id
-     */
-    fun hasSubtitle(resId: Int) {
-        view.check(ViewAssertions.matches(ToolbarSubtitleMatcher(resId)))
-    }
-
-    /**
-     * Checks if Toolbar has navigation drawable
-     *
-     * @param resId expected drawable resource id
-     */
-    fun hasNavigationDrawable(@DrawableRes resId: Int, toBitmap: ((drawable: Drawable) -> Bitmap)? = null) {
-        navigationDrawable?.hasDrawable(resId = resId, toBitmap = toBitmap) ?: throw AssertionError(
-            "Unable to match navigationDrawable, did you forget to provide its builder?"
-        )
+        navigationIcon = navigationIconBuilder?.let { KImageView(it) }
     }
 
     /**
@@ -138,5 +85,20 @@ class KToolbar : ToolbarViewActions, ToolbarViewAssertions, BaseAssertions {
      */
     operator fun invoke(function: KToolbar.() -> Unit) {
         function(this)
+    }
+
+    /**
+     * Infix function for invoking lambda on your view
+     *
+     * Sometimes instance of view is a result of a function or constructor.
+     * In this specific case you can't call invoke() since it will be considered as
+     * tail lambda of your fun/constructor. In such cases please use this function.
+     *
+     * @param function Tail lambda with receiver which is your view
+     * @return This object
+     */
+    infix fun perform(function: KToolbar.() -> Unit): KToolbar {
+        function(this)
+        return this
     }
 }
--- kakao/src/main/kotlin/com/agoda/kakao/toolbar/ToolbarViewAssertions.kt
+++ kakao/src/main/kotlin/com/agoda/kakao/toolbar/ToolbarViewAssertions.kt
@@ -2,56 +2,67 @@
 
 package com.agoda.kakao.toolbar
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import androidx.annotation.DrawableRes
+import androidx.test.espresso.assertion.ViewAssertions
 import com.agoda.kakao.common.assertions.BaseAssertions
+import com.agoda.kakao.common.matchers.ToolbarSubtitleMatcher
+import com.agoda.kakao.common.matchers.ToolbarTitleMatcher
 import com.agoda.kakao.image.ImageViewAssertions
+import com.agoda.kakao.image.KImageView
 
 /**
  * Provides assertion for Toolbar
  */
 interface ToolbarViewAssertions : BaseAssertions, ImageViewAssertions {
+    val navigationIcon: KImageView?
+        get() = null
 
-//    /**
-//     * Checks if Toolbar has title
-//     *
-//     * @param title expected title string
-//     */
-//    fun hasTitle(title: String) {
-//        view.check(ViewAssertions.matches(ToolbarTitleMatcher(title)))
-//    }
-//
-//    /**
-//     * Checks if Toolbar has subtitle
-//     *
-//     * @param subtitle expected subtitle string
-//     */
-//    fun hasSubtitle(subtitle: String) {
-//        view.check(ViewAssertions.matches(ToolbarSubtitleMatcher(subtitle)))
-//    }
-//
-//    /**
-//     * Checks if Toolbar has title
-//     *
-//     * @param resId expected title resource id
-//     */
-//    fun hasTitle(resId: Int) {
-//        view.check(ViewAssertions.matches(ToolbarTitleMatcher(resId)))
-//    }
-//
-//    /**
-//     * Checks if Toolbar has subtitle
-//     *
-//     * @param resId expected subtitle resource id
-//     */
-//    fun hasSubtitle(resId: Int) {
-//        view.check(ViewAssertions.matches(ToolbarSubtitleMatcher(resId)))
-//    }
-//
-//    /**
-//     * Checks if Toolbar has navigation drawable
-//     *
-//     * @param resId expected drawable resource id
-//     */
-//    fun hasNavigationDrawable(resId: Int) {
-//        view.check(ViewAssertions.matches(ToolbarNavigationDrawableMatcher(resId)))
-//    }
+    /**
+     * Checks if Toolbar has title
+     *
+     * @param title expected title string
+     */
+    fun hasTitle(title: String) {
+        view.check(ViewAssertions.matches(ToolbarTitleMatcher(title)))
+    }
+
+    /**
+     * Checks if Toolbar has subtitle
+     *
+     * @param subtitle expected subtitle string
+     */
+    fun hasSubtitle(subtitle: String) {
+        view.check(ViewAssertions.matches(ToolbarSubtitleMatcher(subtitle)))
+    }
+
+    /**
+     * Checks if Toolbar has title
+     *
+     * @param resId expected title resource id
+     */
+    fun hasTitle(resId: Int) {
+        view.check(ViewAssertions.matches(ToolbarTitleMatcher(resId)))
+    }
+
+    /**
+     * Checks if Toolbar has subtitle
+     *
+     * @param resId expected subtitle resource id
+     */
+    fun hasSubtitle(resId: Int) {
+        view.check(ViewAssertions.matches(ToolbarSubtitleMatcher(resId)))
+    }
+
+    /**
+     * Checks if Toolbar has navigation drawable
+     *
+     * @param resId expected drawable resource id
+     */
+    fun hasNavigationDrawable(@DrawableRes resId: Int, toBitmap: ((drawable: Drawable) -> Bitmap)? = null) {
+        navigationIcon?.hasDrawable(resId = resId, toBitmap = toBitmap) ?: throw AssertionError(
+            "Unable to match navigationDrawable, did you forget to provide its builder?"
+        )
+    }
 }
