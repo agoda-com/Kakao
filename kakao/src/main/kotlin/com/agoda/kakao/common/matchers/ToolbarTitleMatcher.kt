84a4893444aa8cbb7382d2294ea2273c0cdd9111
Martin Schumacher <msc@keineantwort.de>
2020-12-21 11:11:55 +0100

    switched to the kotlin way of equality

--- kakao/src/main/kotlin/com/agoda/kakao/common/matchers/ToolbarTitleMatcher.kt
+++ kakao/src/main/kotlin/com/agoda/kakao/common/matchers/ToolbarTitleMatcher.kt
@@ -27,6 +27,6 @@ class ToolbarTitleMatcher() : BoundedMatcher<View, Toolbar>(Toolbar::class.java)
         if (title == null && resId != null) {
             title = resId?.let { view?.context?.getString(it) }
         }
-        return view?.title?.toString().equals(title)
+        return view?.title?.toString() == title
     }
 }
